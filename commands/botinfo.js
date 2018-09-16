4cdc7a25f38ac961ee46cfc39d745b483e5b4065
mekpower <dtayrone1@gmail.com>
2018-09-16 00:30:44 -0300

    botInfo ajustes

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -5,11 +5,11 @@ module.exports.run = (client, message, args, ops) => {
 
     .setAuthor("Kalalho", message.client.avatarURL)
     .addField("Soble min", "Koe, tlaquilo? Eu sou o Kalalho, um bot brasileiro sobre variadas coisas, criado pelo MekPower#6688")
-    .addField("🌎 Servidores",  ` \`\`\`${message.guilds.size}\`\`\`  `,true)
-    .addField("👨‍👨‍👧‍👦 Meus amigos",  ` \`\`\`${message.users.size}\`\`\`  `,true)
-    .addField("💬 Canais",  ` \`\`\`${message.channels.size}\`\`\`  `,true)
+    .addField("🌎 Servidores",  ` \`\`\`${client.guilds.size}\`\`\`  `,true)
+    .addField("👨‍👨‍👧‍👦 Meus amigos",  ` \`\`\`${client.users.size}\`\`\`  `,true)
+    .addField("💬 Canais",  ` \`\`\`${client.channels.size}\`\`\`  `,true)
     .addField("Programação", "<:nodejs:490597898812391424> Node.js", true)
-    .addField("📆 Datas", `Entrei no ${message.guild.name}: \n ${client.guild.joinedAt}`, true)
+    .addField("📆 Datas", `Entrei no ${client.guild.name}: \n ${client.guild.joinedAt}`, true)
 
     message.channel.send(botinfoEmbed);
 
