64cfa391d82f5ce0923cb2d78280c5b5ee0587a9
mekpower <dtayrone1@gmail.com>
2018-09-16 11:09:50 -0300

    BotInfo mais infos

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,14 +1,19 @@
 const Discord = require("discord.js");
 module.exports.run = (client, message, args, ops) => {
+    let join = `\`\`\``;
 
     let botinfoEmbed = new Discord.RichEmbed()
 
-    .setAuthor("Kalalho", message.client.avatarURL)
+    
+    .setAuthor("Kalalho", message.clientUser.avatarURL)
+    .setColor("#ffe100")
     .addField("Soble min", "Koe, tlaquilo? Eu sou o Kalalho, um bot brasileiro sobre variadas coisas, criado pelo MekPower#6688")
     .addField("🌎 Servidores", client.guilds.size,true)
     .addField("👨‍👨‍👧‍👦 Meus amigos", client.users.size,true)
     .addField("💬 Canais", client.channels.size,true)
-    .addField("Programação", "<:nodejs:490597898812391424> Node.js", true);
+    .addField("Programação", "<:nodejs:490597898812391424> Node.js", true)
+    .addField(" Datas:", ` Criado em: \n ${join}${message.clientUser.createdAt}${join} \n Entrei no *${message.guild.name}*: \n ${join}${client.guild.joinedAt}${join}`,true)
+    .addField("Acesse:", ["convite"]("https://goo.gl/WznTJ9") `\n`,true )
 
     message.channel.sendEmbed(botinfoEmbed);
 
