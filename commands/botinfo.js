7647972fd6c4efd151465e25c02944b62af2f602
mekpower <dtayrone1@gmail.com>
2018-09-16 12:05:06 -0300

    mais testes

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,7 +8,7 @@ module.exports.run = (client, message, args, ops) => {
     .setAuthor("Kalalho", client.user.displayAvatarURL)
     .setColor("#ffe100")
     .addField("Soble min", "Koe, tlanquilo? Eu sou o Kalalho, um bot brasileiro sobre variadas coisas, criado pelo **MekPower#6688**")
-    .addField("🌎 Servidores", join.client.guilds.size.join,true)
+    .addField("🌎 Servidores", ` ${join}${client.guilds.size}${join}`,true)
     .addField("👨‍👨‍👧‍👦 Meus amigos", client.users.size,true)
     .addField("💬 Canais", client.channels.size,true)
     .addField("Linguagem", "<:nodejs:490597898812391424> Node.js", true)
