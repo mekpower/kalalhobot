d102c206912f38dfadc4b2929583913dfbfcb87a
mekpower <dtayrone1@gmail.com>
2018-09-16 00:52:04 -0300

    ok

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -5,11 +5,11 @@ module.exports.run = (client, message, args, ops) => {
 
     .setAuthor("Kalalho", message.client.avatarURL)
     .addField("Soble min", "Koe, tlaquilo? Eu sou o Kalalho, um bot brasileiro sobre variadas coisas, criado pelo MekPower#6688")
-    .addField("🌎 Servidores", message.guilds.size,true)
-    .addField("👨‍👨‍👧‍👦 Meus amigos", message.users.size,true)
-    .addField("💬 Canais", message.channels.size,true)
-    .addField("Programação", "<:nodejs:490597898812391424> Node.js", true)
+    .addField("🌎 Servidores", client.guilds.size,true)
+    .addField("👨‍👨‍👧‍👦 Meus amigos", client.users.size,true)
+    .addField("💬 Canais", client.channels.size,true)
+    .addField("Programação", "<:nodejs:490597898812391424> Node.js", true);
 
-    message.channel.send(botinfoEmbed);
+    message.channel.sendEmbed(botinfoEmbed);
 
 }
