22206178750d5680c7177f04bd6d42e3a40e3919
mekpower <dtayrone1@gmail.com>
2018-09-16 12:36:53 -0300

    HyperLink test

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -9,9 +9,10 @@ module.exports.run = (client, message, args, ops) => {
     .setColor("#ffe100")
     .addField("Soble min", "Koe, tlanquilo? Eu sou o Kalalho, um bot brasileiro sobre variadas coisas, criado pelo **MekPower#6688**")
     .addField("🌎 Servidores", ` ${join}${client.guilds.size}${join}`,true)
-    .addField("👨‍👨‍👧‍👦 Meus amigos", client.users.size,true)
-    .addField("💬 Canais", client.channels.size,true)
+    .addField("👨‍👨‍👧‍👦 Meus amigos",` ${join}${client.users.size}${join} `,true)
+    .addField("💬 Canais", ` ${join}${client.channels.size}${join} `,true)
     .addField("Linguagem", "<:nodejs:490597898812391424> Node.js", true)
+    .addField(" Links", ["Site"]("https://kalalhobot.glitch.me"))
 
     message.channel.sendEmbed(botinfoEmbed);
 
