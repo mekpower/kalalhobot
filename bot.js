dd7373d22c1fff1d6e19c9901bae2da5f0b1e762
mekpower <dtayrone1@gmail.com>
2018-09-17 11:06:57 -0300

    Crucificamento por emote

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ client.on("message", async message => {
     resporCanal.send(reportEmbed).then(msg2 => {
         msg2.react('✅');
         msg2.react('❌');
-    const collector = msg2.createReactionCollector((r, u) => (r.emoji.name === '💀') && (u.id !== client.user.id && u.id === msg.author.id))
+    const collector = msg2.createReactionCollector((r, u) => ((r.emoji.name === '✅') || (r.emoji.name === '❌')) && (u.id !== client.user.id && u.id === msg.author.id));
     collector.on("collect", r=>{
         switch (r.emoji.name) {
         case '✅': 
