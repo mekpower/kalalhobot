a8c2c90c26ae3e3648bca3cf81bc293cff9d9188
mekpower <dtayrone1@gmail.com>
2018-09-18 00:56:42 -0300

    Ajuda ajustes

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,5 +1,20 @@
 const Discord = require("discord.js");
+const fs= require("fs");
 
-exports.run = async (client, message, args) => {
-     message.channel.send(`Commands: \n\n${client.commands.map(cmd => `\`${cmd.ajuda.name}\``).join(", ")}`);
+module.exports.run = async (bot, message, args) => {
+
+    var comandosList = fs.readFileSync('./commands/comandos.txt','utf8');
+    let hUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    let ajudaEmbed = new Discord.RichEmbed()
+    .setTitle("Menu de Ajuda 📑")
+    .setThumbnail(client.user.displayAvatarURL)
+    .setColor("#00a9ff")
+    .addField("Comandos de membros", comandosList)
+
+    message.channel.sendEmbed(ajudaEmbed);
+
+}
+
+module.exports.help = {
+    name: "ajuda"
 }
