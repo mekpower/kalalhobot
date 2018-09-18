515faadb55a82224e827196cd71736a5b1254382
mekpower <dtayrone1@gmail.com>
2018-09-17 23:57:31 -0300

    Ajuda comando

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,30 +1,10 @@
 const Discord = require("discord.js");
-const fs= require("fs");
 
-module.exports.run = async (bot, message, args) => {
-
-    var comandosList = fs.readFileSync('./commands/comandos.txt','utf8');
-    let hUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-    let ajudaEmbed = new Discord.RichEmbed()
-    .setTitle("Menu de Ajuda")
-    .setColor("#00a9ff")
-    .addField("Comandos de membros", comandosList)
-
-    message.channel.sendEmbed(ajudaEmbed);
-
-    let modemEmbed = new Discord.RichEmbed()
-    .setTitle("Mod Menu de Ajuda")
-    .addField("Comandos de Mods","");
-
-    try{
-        await message.author.sendEmbed(modemEmbed);
-        message.react("")
-    }catch(e){
-        message.reply("Sua DM está fechada, não consigo enviar!");
+exports.run = async (client, message, args, tools, con) => {
+    try {
+        await message.author.send(`Commands: \n\n${client.commands.map(cmd => `\`${cmd.ajuda.name}\``).join(", ")}`);
+        message.channel.send("Help sent.");
+    } catch (e) {
+        throw e;
     }
-
-}
-
-module.exports.help = {
-    name: "ajuda"
 }
--- commands/bugr.js
+++ commands/bugr.js
@@ -14,7 +14,21 @@ module.exports.run = (client, message, args) =>{
     .addField("Data", message.createdAt)
     .addField("Bug Achado", bug);
 
-    
+    let okEmbed = new Discord.RichEmbed()
+            
+    .setTitle("<:correto:471853582740619284> Bug Resolvido!!")
+    .setColor("#00ff37")
+    .addField("Bug",bug)
+    .addField("Canal", message.channel)
+    .addField("Data", message.createdAt);
+
+    let nayEmbed = new Discord.RichEmbed()
+            
+    .setTitle("<:negado:487113617473273876> Bug Ainda não resolvido!!")
+    .setColor("#a00000")
+    .addField("Bug",bug)
+    .addField("Canal", message.channel)
+    .addField("Data", message.createdAt);
 
     message.channel.send("<:arquivado:491415967880970251> **|** Bug arquivado para verificação, agradeço por ajudar a gente <:mekAmor:489565635806887946>");
     client.guilds.get("486292763667726337").channels.get("491411677015965736").send(bugrEmbed).then(msg2 => {
@@ -24,25 +38,17 @@ module.exports.run = (client, message, args) =>{
     collector.on("collect", r=>{
         switch (r.emoji.name) {
         case '471853582740619284': 
-            let okEmbed = new Discord.RichEmbed()
-            
-            .setTitle("<:correto:471853582740619284> Bug Resolvido!!")
-            .setColor("#00ff37")
-            .addField("Bug",bug)
-            .addField("Canal", message.channel)
-            .addField("Data", message.createdAt);
-            client.guilds.get("486292763667726337").channels.get("491419203434184705").send(okEmbed);
+           
+            client.guilds.get("486292763667726337").channels.get("491419203434184705").send(okEmbed)
         break;
         case '487113617473273876':
-        let nayEmbed = new Discord.RichEmbed()
-            
-            .setTitle("<:negado:487113617473273876> Bug Ainda não resolvido!!")
-            .setColor("#a00000")
-            .addField("Bug",bug)
-            .addField("Canal", message.channel)
-            .addField("Data", message.createdAt);
-            client.guilds.get("486292763667726337").channels.get("491419203434184705").send(nayEmbed);
+        
+            client.guilds.get("486292763667726337").channels.get("491419203434184705").send(nayEmbed)
         }
         })
     }) 
+}
+
+module.exports.ajuda = {
+    name: "bugr"
 }
