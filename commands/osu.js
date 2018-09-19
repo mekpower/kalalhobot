57e7c0f049c53ded7cffacfce62770ec66691b5d
mekpower <dtayrone1@gmail.com>
2018-09-19 20:04:34 -0300

    osu info

--- commands/osu.js
+++ commands/osu.js
@@ -16,8 +16,8 @@ module.exports.run = (client, message, args) =>{
     osuApi.getUser({u: args}).then(user => {
 
         let osuEmbed = new Discord.RichEmbed()
-        .setTitle("Osu Player Info")
-        .setColor("#ff00b6")
+        .setTitle("Osu Player Info", user.avatar)
+        .setColor("#ff84db")
         .setThumbnail("https://orig00.deviantart.net/43b7/f/2015/063/e/e/squared_icon_for_osu___inspired_by_faenza_theme__by_ohmygod1993-d8kcwwm.png")
         .addField("<:User:492106844278358031> Player", `${markCode}${user.name}${markCode}`,true)
         .addField("<:lvlUp:491959868937207810> Level", `${markCode}${user.level}${markCode}`,true)
@@ -28,7 +28,7 @@ module.exports.run = (client, message, args) =>{
         .addField("A", `${markCode}${user.counts.A}${markCode}`, true)
         .addField("<:MusicRecord:492106791958740993> Plays", `${markCode}${user.counts.plays}${markCode}`, true)
         .addField("PP", `${markCode}${user.pp.raw}${markCode}`, true)
-        .addField("<:Prize:492105804724305920> Rank", `${markCode}${user.counts.rank}${markCode}`, true);
+        .addField("<:Prize:492105804724305920> Rank", `${markCode}${user.pp.rank}${markCode}`, true);
         
         
         message.channel.send(osuEmbed);
