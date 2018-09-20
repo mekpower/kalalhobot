f61256c9221064185f9858aaa3d3cf4019aa222b
mekpower <dtayrone1@gmail.com>
2018-09-20 02:32:04 -0300

    .

--- commands/tocar.js
+++ commands/tocar.js
@@ -37,8 +37,9 @@ exports.run = (client, message, args, ops) => {
                     message.edit(`:arrow_forward: Tocando agora: \`${videos[m-1].title}\` | Pedida por:  `+message.author.username);
                 break;
                 case '⏩':
-                let commandParar = require(`./pular.js`);
+                let commandParar = require(`./parar.js`);
                     commandParar.run(client, message, args);
+                    message.edit(`<:ExitSign:492205962401284096> | Dando o daleste. Musica tocada: \`${videos[m-1].title}\``);
                     break;
                 case 'adsdsa':
                 let commandP = require(`./pular.js`);
