6f9a4ca3dd9194fc91f6d7a0b3c072719b42347e
mekpower <dtayrone1@gmail.com>
2018-09-20 10:31:30 -0300

    .

--- commands/frita.js
+++ commands/frita.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args) => {
 
-    message.channel.send( `iiiihhuuu` );
-    message.channel.send(" <:PepoDance:492317008218685440> <:blobdance:492319755290804227> <:blobdance:492319755290804227> <:nekodance:492320001890582539> <:PepoDance:492317008218685440> <:blobdance:492319755290804227> <:nekodance:492320001890582539> <:PepoDance:492317008218685440> ");
+    message.channel.send("iiiihhuuu", {
+        file: "http://is2.4chan.org/qa/1534610495622.gif"
+       })
+    
 }
