10662e3ffae310cba09546c2119b16ba08b94481
mekpower <dtayrone1@gmail.com>
2018-09-21 22:10:00 -0300

    xp com .json

--- bot.js
+++ bot.js
@@ -131,6 +131,38 @@ client.on("message", async message => {
             }
 
     }
+
+
+    let xpAdd = Math.floor(Math.random() * 7) + 8;
+    console.log(xpAdd);
+
+    if (!xp[message.author.id]) {
+        xp[message.author.id] = {
+            xp: 0,
+            level: 1
+        };
+    }
+
+
+    let curxp = xp[message.author.id].xp;
+    let curlvl = xp[message.author.id].level;
+    let nxtLvl = xp[message.author.id].level * 300;
+    xp[message.author.id].xp = curxp + xpAdd;
+    if (nxtLvl <= xp[message.author.id].xp) {
+        xp[message.author.id].level = curlvl + 1;
+        let lvlup = new Discord.RichEmbed()
+            .setTitle("Level Up!")
+            .addField("Congrats to", `${message.author}`)
+            .setColor("#08ff00")
+            .addField("Level Atual", curlvl + 1);
+
+        message.channel.send(lvlup);
+    }
+    fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+                if (err) console.log(err)
+    });
+
+
     if(comando === "reports"){
         let reporUs = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!reporUs) return message.channel.send("Não achei esse cabra, cadê ele??!!");
---
+++ commands/xp.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+let xp = require("../xp.json");
+
+module.exports.run = async (client, message, args, con) => {
+
+    if (!xp[message.author.id]) {
+        xp[message.author.id] = {
+            xp: 0,
+            level: 1
+        };
+    }
+    let curxp = xp[message.author.id].xp;
+    let curlvl = xp[message.author.id].level;
+    let nxtLvlXp = curlvl * 300;
+    let difference = nxtLvlXp - curxp;
+
+    let lvlEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor("#880000")
+        .addField("Level", curlvl, true)
+        .addField("XP", curxp, true)
+        .setFooter(`${difference} XP para o próximo lvl`, message.author.displayAvatarURL);
+
+    message.channel.send(lvlEmbed);
+
+}
