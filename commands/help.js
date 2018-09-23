963d530c678d3b588621a1b374360ab77f24342b
mekpower <dtayrone1@gmail.com>
2018-09-23 12:14:52 -0300

    .

--- commands/help.js
+++ commands/help.js
@@ -1,82 +1,20 @@
 const Discord = require("discord.js");
+const fs= require("fs");
 
-module.exports.run = (client, message, args) =>{
-	
-	let menuEmbed = new Discord.RichEmbed()
-	
-	.setTitle("Help Menu")
-	.setColor('36393e')
-	.setDescription("Cada emote é uma opção, use as reações para ir para tal menu")
-	.addField("Reações", 'Infos = <:BulletedList:492195623962017802> \n Games = <:R2D2:492193706909106176> \n Entreteimento = <:Chat:492191006729437184> \n Música = <:MusicRecord:492106791958740993>')
-	.setFooter(`Comando solicitado por: ${message.author.username}`, message.author.avatarURL)
-	.setTimestamp();
-	
-	message.channel.send(menuEmbed).then(message => {
-   message.react('492195623962017802')
-   message.react('492193706909106176')
-   message.react('492191006729437184')
-   message.react('492106791958740993')
-   
-var servidor = (reaction, user) => reaction.emoji.name === ':BulletedList' && user.id !== message.author.id
-var games = (reaction, user) => reaction.emoji.name === ':R2D2:' && user.id !== message.author.id
-var aleatorio = (reaction, user) => reaction.emoji.name === ':Chat:' && user.id !== message.author.id
-var musica = (reaction, user) => reaction.emoji.name === ':MusicRecord:' && user.id !== message.author.id
-})
+module.exports.run = async (client, message, args) => {
 
-client.on('messageReactionAdd', servidor => { 
+    var comandosList = fs.readFileSync('./commands/comandos.txt','utf8');
+    let hUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    let ajudaEmbed = new Discord.RichEmbed()
+    .setTitle("<:BulletedList:492195623962017802> Menu de Ajuda")
+    .setThumbnail(client.user.displayAvatarURL)
+    .setColor("#00a9ff")
+    .addField("Comandos de membros", `<:MoreThan:492193729646428180> \` IMPORTANT: This bot is Brazilian, most of the commands will be in PORTUGUESE. \` \n ${comandosList}`)
+    .setFooter("Prefix: k&")
+    message.channel.sendEmbed(ajudaEmbed);
 
-        var embed1 = new Discord.RichEmbed()
+}
 
-        .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .setTitle("Infos comandos")
-        .setDescription("Comandos de info e como usar alguns. \n `ping` , `serverinfo` , `level` , `botinfo` \n `reports` :Ex.: k&reports <@usuario> <motivo_do_report> \n `bugr` :Ex.: k&bugr <nome_do_comando> <problema_do_comando> \n `anime` :Ex>: k&anime <nome_do_anime>") 
-        .setColor(0xfffff)
-        .setFooter(client.user.username, client.user.avatarURL)
-		.setTimestamp();
-
-    message.edit(embed1)
-})
-
-client.on('messageReactionAdd', games => { 
-
-        var embed2 = new Discord.RichEmbed()
-
-        .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .setTitle("Games comandos")
-        .setDescription("Comandos e como usar. \n `osu` :Ex.: k&osu <nome_do_jogador> \n `fort` :Ex.: k&fort <nome_do_jogador>") 
-        .setColor(0xfffff)
-        .setFooter(client.user.username, client.user.avatarURL)
-		.setTimestamp();
-
-    message.edit(embed2)
-})
-
-client.on('messageReactionAdd', aleatorio => { 
-
-        var embed3 = new Discord.RichEmbed()
-
-        .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .setTitle("Comandos de entretenimento")
-        .setDescription("Comandos simples, apenas digitálos normalmente. \n `frita` , `troll` , `change` , `proverbio`") 
-        .setColor(0xfffff)
-        .setFooter(client.user.username, client.user.avatarURL)
-		.setTimestamp();
-
-    message.edit(embed3)
-})
-
-client.on('messageReactionAdd', musica => { 
-
-        var embed4 = new Discord.RichEmbed()
-
-        .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .setTitle("Comandos de música")
-        .setDescription("Comandos e como usar. \n `tocar` :Ex.: k&osu <nome_da_musica \n `pular`") 
-        .setColor(0xfffff)
-        .setFooter(client.user.username, client.user.avatarURL)
-		.setTimestamp();
-
-    message.edit(embed4)
-})
-
+module.exports.help = {
+    name: "ajuda"
 }
