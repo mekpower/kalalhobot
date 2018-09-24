8a8e2e1ba2821b994894d89b10cf91ea1ee2bf44
mekpower <dtayrone1@gmail.com>
2018-09-23 23:29:46 -0300

    .

--- commands/help.js
+++ commands/help.js
@@ -59,19 +59,19 @@ module.exports.run = async (client, message, args) => {
             msg2.react(game);
 			msg2.react(aleatorio);
 			msg2.react(musica);
-        const collector = msg2.createReactionCollector((r, u) => (r.emoji.name === paper || r.emoji.name === game || r.emoji.name === aleatorio || r.emoji.name === musica) && (u.id !== client.user.id && u.id === message.author.id))
+        const collector = msg2.createReactionCollector((r, u) => (r.emoji.name === 'paper' || r.emoji.name === 'game' || r.emoji.name === 'aleatorio' || r.emoji.name === 'musica') && (u.id !== client.user.id && u.id === message.author.id))
         collector.on("collect", (r, u)=>{
             switch (r.emoji.name) {
-            case paper:
+            case 'paper':
             r.message.edit(embed1)
 			break;
-            case game: 
+            case 'game': 
             r.message.edit(embed2)
 			break;
-            case aleatorio: 
+            case 'aleatorio': 
             r.message.edit(embed3)
 			break;
-            case musica: 
+            case 'musica': 
             r.message.edit(embed4)
             break;
             }
