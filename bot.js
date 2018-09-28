58ef9fe34b6c5993597883000f0bc546cde6ddc4
mekpower <dtayrone1@gmail.com>
2018-09-28 12:13:47 -0300

    .

--- bot.js
+++ bot.js
@@ -179,7 +179,7 @@ client.on("message", async message => {
 
     if(comando === "frita"){
         message.channel.send("iiiihhuuu");
-    message.channel.send("<:PepoDance:490595813001527307> <:CatDance:492319020926894080> <:flippyy:490592500130643968> <:flippyy:490592500130643968> <:flippyy:490592500130643968> <:CatDance:492319020926894080> <:PepoDance:490595813001527307>");
+    message.channel.send("<a:PepoDance:490595813001527307> <a:CatDance:492319020926894080> <a:flippyy:490592500130643968> <a:flippyy:490592500130643968> <a:flippyy:490592500130643968> <a:CatDance:492319020926894080> <a:PepoDance:490595813001527307>");
     }
 
     const argsE = message.content.split(" ").slice(1);
