274adb9faf45af593e0a3e92cf5aa483aedabf68
mekpower <dtayrone1@gmail.com>
2018-09-28 12:15:43 -0300

    .

--- commands/frita.js
+++ commands/frita.js
@@ -1,6 +1,4 @@
 
 module.exports.run = (client, message, args) => {
 
-    message.channel.send("iiiihhuuu");
-    message.channel.send("<:PepoDance:490595813001527307> <:CatDance:492319020926894080> <:flippyy:490592500130643968> <:flippyy:490592500130643968> <:flippyy:490592500130643968> <:CatDance:492319020926894080> <:PepoDance:490595813001527307>");
 }
