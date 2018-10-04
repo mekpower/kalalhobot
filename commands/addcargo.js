df47e9c3e7802f84e192b3a508590b53b8807592
mekpower <dtayrone1@gmail.com>
2018-10-04 11:20:16 -0300

    .

--- commands/addcargo.js
+++ commands/addcargo.js
@@ -5,34 +5,35 @@ const a = new Discord.RichEmbed()
 .setTitle("ReactRoles")
 .setDescription("Clique na reação para conseguir a role")
 message.channel.send(a).then(msg => {
-    msg.react(":computer:")
-    msg.react(":gear:")
-    msg.react(":desktop:")
-    msg.react(":top:")
+    msg.react("💻")
+    msg.react("⚙")
+    msg.react("🖥")
+    msg.react("☕")
+    msg.react("🔝")
     })
 }
 client.on('messageReactionAdd', (reaction, user) => {
-    if(reaction.emoji.name == ":computer:" && user.id !== client.user.id){
+    if(reaction.emoji.name == "💻" && user.id !== client.user.id){
         let gRole = message.guild.roles.find(c => c.name == '<./devHelper.js> 💻').id
         message.member.addRole(gRole)
     }
 
-    if(reaction.emoji.name == ":gear:" && user.id !== client.user.id){
+    if(reaction.emoji.name == "⚙" && user.id !== client.user.id){
         let gRole = message.guild.roles.find(c => c.name == '<./devHelper.ino> ⚙️').id
         message.member.addRole(gRole)
     }
 
-    if(reaction.emoji.name == ":desktop:" && user.id !== client.user.id){
+    if(reaction.emoji.name == "🖥" && user.id !== client.user.id){
         let gRole = message.guild.roles.find(c => c.name == '<./devHelper.c> ©️').id
         message.member.addRole(gRole)
     }
 
-    if(reaction.emoji.name == ":coffee:" && user.id !== client.user.id){
+    if(reaction.emoji.name == "☕" && user.id !== client.user.id){
         let gRole = message.guild.roles.find(c => c.name == '<./devHelper.jar> ☕️').id
         message.member.addRole(gRole)
     }
 
-    if(reaction.emoji.name == ":top:" && user.id !== client.user.id){
+    if(reaction.emoji.name == "🔝" && user.id !== client.user.id){
         let gRole = message.guild.roles.find(c => c.name == 'haigui elu 🔝').id
         message.member.addRole(gRole)
     }
