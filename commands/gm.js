e328aa00a6493d5703c5b66d59fb6b59b13853dd
mekpower <dtayrone1@gmail.com>
2018-10-05 02:34:20 -0300

    .

--- commands/gm.js
+++ commands/gm.js
@@ -17,11 +17,11 @@ exports.run = (client, message, args) => {
         .setTimestamp()
         .setFooter(`${message.author.username}#${message.author.discriminator}`,message.author.displayAvatarURL);
 
-        if(args[0] == "lol" || "league of legends") a.addField("Game", `<:LeagueofLegends:497632559468118037> ${args[0]}`, true);
-        else if(args[0] == "pubg" || "PUBG" || "pub" || "PUB") a.addField("Game", `<:PUBG:497632603864563712> ${args[0]}`, true);
-        else if(args[0] == "fortnite" || "Fortnite") a.addField("Game", `<:Fortnite:492468035559620628> ${args[0]}`, true);
-        else if(args[0] == "Overwatch" || "overwatch") a.addField("Game", `<:Overwatch:497632658545967154> ${args[0]}`, true);
-        else if(args[0] == "Minecraft" || "minecraft") a.addField("Game", `<:Minecraft:497632635997126656> ${args[0]}`, true);
+        if(args[0] === "lol" || "league of legends") {a.addField("Game", `<:LeagueofLegends:497632559468118037> ${args[0]}`, true);}
+        else if(args[0] === "pubg" || "PUBG" || "pub" || "PUB") {a.addField("Game", `<:PUBG:497632603864563712> ${args[0]}`, true);}
+        else if(args[0] === "fortnite" || "Fortnite") {a.addField("Game", `<:Fortnite:492468035559620628> ${args[0]}`, true);}
+        else if(args[0] === "Overwatch" || "overwatch") {a.addField("Game", `<:Overwatch:497632658545967154> ${args[0]}`, true);}
+        else if(args[0] === "Minecraft" || "minecraft") {a.addField("Game", `<:Minecraft:497632635997126656> ${args[0]}`, true);}
         else a.addField("Game", `${args[0]}`, true);
 
         a.addField("Vagas", `${args[1]}`, true);
