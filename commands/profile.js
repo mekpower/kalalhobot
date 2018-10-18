778b8fc8909f7cf888b5e9bc3b856e1e7d460694
mekpower <dtayrone1@gmail.com>
2018-10-18 10:29:58 -0300

    .

--- commands/profile.js
+++ commands/profile.js
@@ -4,7 +4,7 @@ const db = require('quick.db');
 exports.run = async (client, message, args) => {
 	let user = message.author;
 	
-	let xp = await db.fetch(`exp_${user.id}`);
+	let exp = await db.fetch(`exp_${user.id}`);
 	if(exp === null) exp = 0;
 	let level = await db.fetch(`level_${user.id}`);
 	if(level === null) level = 0;
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
 	.setColor("#be41f4")
 	.setAuthor("RANK: "+ user.username, user.avatarURL)
 	.addField("LEVEL", level)
-	.addField("XP", xp)
+	.addField("XP", exp)
     .setTimestamp()
     
     message.channel.send(perf);
