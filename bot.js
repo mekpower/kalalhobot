af8cd5c2b4cc61d92af619a6473b00dd84061c2b
mekpower <dtayrone1@gmail.com>
2018-10-18 10:10:33 -0300

    .

--- bot.js
+++ bot.js
@@ -121,16 +121,16 @@ client.on("message", async message => {
    //LEVEL SYSTEM
    let user = message.author;
 
-   let exp =  db.fetch(`exp_${user.id}`);
+   let exp = await db.fetch(`exp_${user.id}`);
    if(exp === null) exp = 0;
-   let level = db.fetch(`level_${user.id}`);
+   let level = await db.fetch(`level_${user.id}`);
    if(level === null) level = 0;
    
    let nxtLevel = exp * 300;
    
    if(!cooldown.is(user.id)){
        cooldown.add(user.id);
-       db.add(`exp_${user.id}`, generateXP());
+       db.add(`exp_${user.id}`, generateXp());
        
        setTimeout(() =>{
            cooldown.remove(user.id);
@@ -141,8 +141,8 @@ client.on("message", async message => {
        if(xp >= nxtLevel){
            
            db.add(`level_${user.id}`, 1);
-           xp =  db.fetch(`exp_${user.id}`);
-           level = db.fetch(`level_${user.id}`);
+           xp = await db.fetch(`exp_${user.id}`);
+           level = await db.fetch(`level_${user.id}`);
            
            let Lembed = new Discord.RichEmbed()
            .setColor("#42f4bc")
--- commands/profile.js
+++ commands/profile.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 	let user = message.author;
 	
-	let xp =  db.fetch(`exp_${user.id}`);
+	let xp = await db.fetch(`exp_${user.id}`);
 	if(exp === null) exp = 0;
-	let level =  db.fetch(`level_${user.id}`);
+	let level = await db.fetch(`level_${user.id}`);
 	if(level === null) level = 0;
 	
 	let perf = new Discord.RichEmbed()
